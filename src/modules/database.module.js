f10c22d55c2a582c724106fbef7900ebb02d0f73
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-01-24 18:51:24 +0100

    Add database module

---
+++ src/modules/database.module.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('../../config.json');
+
+const InitiateDatabase = async () => {
+    let mongo_uri = "mongodb://";
+    mongo_uri = config.database.passwordProtected ? mongo_uri + config.database.user + ':' + config.database.password + '@' : mongo_uri;
+    mongo_uri += config.database.host + ":" + config.database.port + "/" + config.database.database_name;
+    try{
+        await mongoose.connect(mongo_uri, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        console.log("connected");
+    } catch (e){
+        console.log(e);
+        throw e;
+    }
+}
+
+module.exports = InitiateDatabase;
